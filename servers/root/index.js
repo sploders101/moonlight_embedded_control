ccd332a08a58d7a1e02c78d824a782b29bbac346
Shaun Keys <mariobuddy@gmail.com>
2017-10-01 22:48:35 -0600

    more cec stuff for CH+ and CH-

--- servers/root/index.js
+++ servers/root/index.js
@@ -32,11 +32,17 @@ function stop() {
 //Setup CEC control and renaming
 //>> source deactivated:
 //>> source activated:
+//key pressed: channel down
+//key pressed: channel up
 cecClient = spawn("cec-client",["-o","GamestreamPi","-t","p"]);
 cecClient.stdout.on("data",function(data) {
     if(data.indexOf(">> source deactivated:")>-1) {
         stop();
     } else if (data.indexOf(">> source activated:")>-1) {
+        start();
+    } else if(data.indexOf("key pressed: channel down")>-1) {
+        stop();
+    } else if (data.indexOf("key pressed: channel up")>-1) {
         start();
     }
 });
