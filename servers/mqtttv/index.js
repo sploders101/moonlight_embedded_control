256d7d48961c097f8ec303dd594de43ef6ed27fe
Shaun Keys <mariobuddy@gmail.com>
2017-10-15 11:44:46 -0600

    Added debug messages to track down the issue

--- servers/mqtttv/index.js
+++ servers/mqtttv/index.js
@@ -7,14 +7,18 @@ let client = mqtt.connect("mqtt://openhabianpi.local");
 
 client.on("connect",function() {
     client.subscribe("lrtv");
+    console.log("connected and subscribed");
 });
 client.on("message",function(topic,message) {
+    console.log("received "+message+" from topic "+topic);
     if(topic=="lrtv" && message=="on") {
+        console.log("tv on");
         http.request({
             host: "127.0.0.1",
             path: "/tvon"
         });
     } else if (topic=="lrtv" && message=="off") {
+        console.log("tv off");
         http.request({
             host: "127.0.0.1",
             path: "/tvoff"
